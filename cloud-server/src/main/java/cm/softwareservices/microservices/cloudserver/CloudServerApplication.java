3c0d6ca02411d65ec2f4a70542946633ad57665d
stephenwilly <45210322+stephenwi@users.noreply.github.com>
2022-07-29 15:10:25 +0200

    Initial commit

---
+++ cloud-server/src/main/java/cm/softwareservices/microservices/cloudserver/CloudServerApplication.java
@@ -0,0 +1,15 @@
+package cm.softwareservices.microservices.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerApplication.class, args);
+	}
+
+}
